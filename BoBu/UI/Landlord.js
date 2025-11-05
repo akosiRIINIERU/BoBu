fdf1fb8970f5155afc04ca22baa5a7ff6c1e763f
akosiRIINIERU <maghanoy.renhiel765@gmail.com>
2025-11-05 12:25:55 +0800

    addedd landlord

---
+++ BoBu/UI/Landlord.js
@@ -0,0 +1,12 @@
+// Dashboard.js
+//examples
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Landlord() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>landlord Screen</Text>
+    </View>
+  );
+}
